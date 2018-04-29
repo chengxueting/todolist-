9b5b9a197f31296ab9e9107c38a6c15dd400c855
chengxueting <1834759669@qq.com>
2018-04-29 09:09:59 +0800

    Update store.js

--- jquery清单列表/store.js
+++ jquery清单列表/store.js
@@ -26,7 +26,7 @@ add_task.on('submit',function(e){
 
 function init(){
     task_list=store.get('task_value')
-    if(task_list.length){
+    if(task_list.length!=0){
         render_task()
     }
 }
